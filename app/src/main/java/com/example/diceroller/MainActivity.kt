0de07f58e9b5aa3c7f318c2b2f776074cf3f6758
Romero Hutapea <61641738+romerojhh@users.noreply.github.com>
2022-03-18 22:54:38 -0700

    added lucky functionalities

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 val SIDES: Int = 6
+val LUCKY1 = 1
+val LUCKY2 = 2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +22,6 @@ class MainActivity : AppCompatActivity() {
         // Set listener
         rollButton.setOnClickListener {
             rollDice()
-            Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -28,8 +29,15 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(SIDES)
         val resultTextView: TextView = findViewById(R.id.textView)
         val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView.text = dice.roll().toString()
-        resultTextView2.text = dice.roll().toString()
+        val tv1Val = dice.roll()
+        val tv2Val = dice.roll()
+
+        if (tv1Val == LUCKY1 && tv2Val == LUCKY2) {
+            Toast.makeText(this, "You're LUCKY!", Toast.LENGTH_SHORT).show()
+        }
+
+        resultTextView.text = tv1Val.toString()
+        resultTextView2.text = tv2Val.toString()
     }
 }
 
